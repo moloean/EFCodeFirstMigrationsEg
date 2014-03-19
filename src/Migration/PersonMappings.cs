553ffa31f7f7d1f3b2cba0b3072368ae0079f469
moloean <moloean@gmail.com>
2014-03-19 22:47:43 +0100

    added back personMappings file

---
+++ src/Migration/PersonMappings.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Model;
+
+namespace Migration.Mappings
+{
+    public class PersonMappings : EntityTypeConfiguration<Person>
+    {
+        public PersonMappings()
+        {
+            HasKey(x => x.IdentityNumber).Property(x => x.IdentityNumber)
+                .HasMaxLength(11)
+                .IsFixedLength();
+        }
+         
+    }
+}
